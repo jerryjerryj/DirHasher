c4406a20f0e913406f097cb16a71dc1c34e297c9
Jerry <jerryjet@mail.ru>
2018-09-15 00:50:28 +0500

    dfgsgetwgs

---
+++ Shared/Base/Transactions/AddHashQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ThreadsTest.Shared.Base.Params;
+using System.Data.OleDb;
+using Dapper;
+namespace ThreadsTest.Shared.Base.Transactions
+{
+	class AddHashQuery<TQueryParams> : BaseQuery<TQueryParams> where TQueryParams : HashParams
+	{
+		protected override void ExecuteInternal(OleDbTransaction oleDbTransaction, TQueryParams queryParams)
+		{
+			string query = "INSERT INTO hashes (path, hash, scan_id) Values(?Path?,?Hash?,?ScanId?)";
+			oleDbTransaction.Connection.Execute(query,
+				new { Path = queryParams.path,
+					Hash = queryParams.hash,
+					ScanId = queryParams.scanId
+				}, oleDbTransaction);
+		}
+	}
+}
---
+++ Shared/Params/HashedFileParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadsTest.Shared
+{
+	public class HashedFileParams
+	{
+		public string path { get; set; }
+		public string hash { get; set; }
+	}
+}
